0c67449300e0da503467aa9d95bad061c27a420e
InvestLee <dlwlgud216@nate.com>
2025-03-20 23:54:09 +0900

    최초 생성

---
+++ src/main/java/com/investlee/ad_data_migration/AdDataMigrationApplication.java
@@ -0,0 +1,13 @@
+package com.investlee.ad_data_migration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdDataMigrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdDataMigrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/investlee/ad_data_migration/AdDataMigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.investlee.ad_data_migration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdDataMigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
