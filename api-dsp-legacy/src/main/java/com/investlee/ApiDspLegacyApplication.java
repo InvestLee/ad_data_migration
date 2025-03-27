ac6da2ebffc336e0c62826b210586da6e0dc5582
InvestLee <dlwlgud216@nate.com>
2025-03-27 23:41:43 +0900

    dsp legacy 모듈 추가

---
+++ api-dsp-legacy/src/main/java/com/investlee/ApiDspLegacyApplication.java
@@ -0,0 +1,11 @@
+package com.investlee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDspLegacyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiDspLegacyApplication.class, args);
+    }
+}
