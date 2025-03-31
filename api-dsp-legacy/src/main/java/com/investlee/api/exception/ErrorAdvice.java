bc91b0969c0b2ad49bb8c5a7d1dd8acd2a19ead0
InvestLee <dlwlgud216@nate.com>
2025-03-31 22:22:21 +0900

    Swagger 추가

--- api-dsp-legacy/src/main/java/com/investlee/api/exception/ErrorAdvice.java
+++ api-dsp-legacy/src/main/java/com/investlee/api/exception/ErrorAdvice.java
@@ -1,12 +1,15 @@
 package com.investlee.api.exception;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.reactive.result.method.annotation.ResponseEntityExceptionHandler;
 
+@Hidden
 @RestControllerAdvice
-public class ErrorAdvice {
+public class ErrorAdvice extends ResponseEntityExceptionHandler {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<?> handleNotFoundException() {
         return ResponseEntity.notFound().build();
